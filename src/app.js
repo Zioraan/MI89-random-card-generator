e42d3e3db6abb88002613f2ff40baf631c2ca4c7
Ryan Castanier <167449198+Zioraan@users.noreply.github.com>
2026-02-18 16:46:14 +0000

    Created base version of card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateNewCard();
+  const randomButton = document.querySelector("#draw-button");
+  randomButton.addEventListener("click", () => {
+    generateNewCard()
+  })
 };
+
+function generateNewCard(){
+  const suits = [
+    {suit: "♦", color: "red"}, 
+    {suit: "♥", color: "red"}, 
+    {suit: "♠", color: "black"},
+    {suit: "♣", color: "black"}
+  ];
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const chosenSuit = suits[getRandomIndex(suits)];
+  const chosenValue = values[getRandomIndex(values)];
+
+  const HTMLSuits = document.querySelectorAll(".suit");
+  console.log(HTMLSuits)
+  for(let suit of HTMLSuits){
+    suit.textContent = chosenSuit.suit;
+    suit.style.color = chosenSuit.color;
+  }
+  const HTMLValue = document.querySelector(".card-value");
+  console.log(HTMLValue)
+  HTMLValue.textContent = chosenValue;
+  return
+}
+
+function getRandomIndex(arr){
+  return Math.floor(Math.random() * arr.length)
+}
